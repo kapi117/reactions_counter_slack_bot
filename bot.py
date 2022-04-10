201b8f4fce0a71e92d602226d79b428e1c67e219
Kacper Iwicki <kacper.iwi@gmail.com>
2022-04-10 02:01:06 +0200

    Dodanie pingowania

--- bot.py
+++ bot.py
@@ -29,11 +29,15 @@ app = App(token=os.environ.get("SLACK_TOKEN"))
 def shortcut_count(client, ack, respond, payload):
     # odpowiadamy że otrzymaliśmy
     ack()
+    # print(payload)
 
     # W celu charakteryzacji wiadomości
     timestamp = payload["message_ts"]
     # ID kanału
     channel_id = payload["channel"]["id"]
+    # Link do wiadomości
+    link_to_message = client.chat_getPermalink(
+        channel=channel_id, message_ts=timestamp)["permalink"]
 
     # pobieranie reakcji z tej wiadomości
     reactions_response = client.reactions_get(
@@ -44,7 +48,8 @@ def shortcut_count(client, ack, respond, payload):
     # pobranie listy użytkowników kanału
     members = client.conversations_members(channel=channel_id)["members"]
 
-    summary = "*Podsumowanie reakcji na wiadomość*\n"
+    summary = "*Podsumowanie reakcji na wiadomość " + \
+        str(link_to_message) + " *\n"
     reaction_counter = 0
     for reaction_type in reactions:
         reaction_counter += 1
@@ -53,12 +58,14 @@ def shortcut_count(client, ack, respond, payload):
             + ": \n\t- Ilość odpowiedzi: " + str(reaction_type['count']) +\
             "\n\t- Kto głosował:\n"
         for user in reaction_type["users"]:
-            act_usr = client.users_info(user=user)
-            summary += "\t\t" + str(act_usr["user"]["name"]) + "\n"
+            #act_usr = client.users_info(user=user)
+            #summary += "\t\t<@" + str(act_usr["user"]["name"]) + ">\n"
+            summary += "\t\t<@" + str(user) + ">\n"
             if user in members:
                 members.remove(user)
     # w members są też boty które trzeba odliczyć żeby wiedzieć czy wszyscy odpowiedzieli
     boty = 0
+    users_to_ping = []
     if len(members) > 0:
         # sprawdzamy kto nie odpowiedział
         summary += "Natomiast niewdzięcznicy którzy nie odpowiedzieli to:\n"
@@ -67,13 +74,29 @@ def shortcut_count(client, ack, respond, payload):
             if "bot_id" in act_usr["user"]["profile"]:
                 boty += 1
                 continue
-            summary += str(act_usr["user"]["name"]) + "\n"
+            #summary += "\t- " + str(act_usr["user"]["name"]) + "\n"
+            summary += "\t\t<@" + str(user) + ">\n"
+            users_to_ping.append(user)
     if len(members) == boty:
         summary += "Ja pierdziu ale Ty masz posłuch, każdy odpowiedział!!! :party_blob:"
 
     # wysyłamy wiadomość widoczna tylko dla tej osoby
     respond(response_type="ephemeral", text=summary)
     print("Pomyslnie wykonano zliczanie reakcji")
+
+    ping_users(client, users_to_ping, link_to_message)
+
+
+def ping_users(client, users, message_link):
+    ping_channel_name = "#ping"
+    # oznaczamy kogoś za pomocą <@usr_id>
+    if len(users) > 0:
+        ping_message = "*Pan Policjant porządku pilnuje, wursowiczów na slacku wciąż pinguje*\nDelikwenci którzy nie odpowiedzieli na wiadomość: " + \
+            str(message_link) + " to:\n"
+        for user in users:
+            ping_message += "\t<@" + user + ">\n"
+
+    client.chat_postMessage(channel=ping_channel_name, text=ping_message)
 
 
 # Start your app
