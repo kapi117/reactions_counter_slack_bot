dce93fdfbd88dbb3287acbef5337a81f59c363ac
Kacper Iwicki <kacper.iwi@gmail.com>
2023-07-10 19:40:52 +0200

    Fixed window opening and statistics after changes

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-'''
+"""
     Autor: Kacper Iwicki
     Mail: kacper.iwi@gmail.com
-'''
+"""
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -11,10 +11,10 @@ import json
 # my modules
 from utils.requests import RequestMetadata
 
-with open('./assets/strings/user_strings.json', 'r') as f:
+with open("./assets/strings/user_strings.json", "r") as f:
     STRINGS_USER = json.load(f)
 
-with open('./assets/strings/utils_strings.json', 'r') as f:
+with open("./assets/strings/utils_strings.json", "r") as f:
     STRINGS_UTILS = json.load(f)
 
 app = App(token=STRINGS_USER["general"]["bot_token"])
@@ -28,6 +28,7 @@ SUMMARY_NAME = "summary"
 MEMBERS_NAME = "members"
 USERS_TO_PING_NAME = "users_to_ping"
 
+
 ### SHORTCUT REQUEST ###
 @app.shortcut("analyse_reaction")
 def shortcut_count(ack, respond, payload):
@@ -39,56 +40,74 @@ def shortcut_count(ack, respond, payload):
     trigger_id = payload["trigger_id"]
     open_main(trigger_id, req_meta)
 
+
 def open_main(trigger_id, req_meta):
     reactions_menu = create_reactions_menu(req_meta.reactions)
 
     replacements = {"{{reactions_menu}}": f"[{reactions_menu}]"}
 
-    view = load_modal(STRINGS_UTILS["modals"]["main"]["file"], replacements, req_meta)
+    req_meta_str = req_meta.to_string()
+
+    view = load_modal(
+        STRINGS_UTILS["modals"]["main"]["file"], replacements, req_meta_str
+    )
 
     app.client.views_open(trigger_id=trigger_id, view=view)
     print("Otwarto okienko")
+
 
 def create_reactions_menu(reactions):
     reactions_menu = ""
     count = 0
     for reaction_type in reactions:
-        if(count > 0):
-            reactions_menu += ''',
-            '''
-        reactions_menu += get_reaction_entry(reaction_type['name'])
+        if count > 0:
+            reactions_menu += """,
+            """
+        reactions_menu += get_reaction_entry(reaction_type["name"])
         count += 1
 
     return reactions_menu
 
+
 def get_reaction_entry(reaction_name):
-    entry = ''' {
+    entry = (
+        """ {
             "text": {
                 "type": "plain_text",
-                "text": ":''' + reaction_name + ''': ''' + reaction_name + '''",
+                "text": ":"""
+        + reaction_name
+        + """: """
+        + reaction_name
+        + '''",
                 "emoji": true
             },
-            "value": "''' + reaction_name + '''"
-        }'''
+            "value": "'''
+        + reaction_name
+        + """"
+        }"""
+    )
     return entry
-    
+
+
 def load_modal(modal_json_filename, repl_in, req_meta):
     result_modal = ""
-    
+
     with open(modal_json_filename, "r") as modal_file:
         modal_text = modal_file.read()
 
         result_modal = replace_tags_in_text(modal_text, repl_in)
 
-    result_modal = load_private_metadata_to_modal_text(result_modal, req_meta.to_json())
+    result_modal = load_private_metadata_to_modal_text(result_modal, req_meta)
 
     return result_modal
+
 
 def replace_tags_in_text(text, repl_dict):
     replacements = dict((re.escape(k), v) for k, v in repl_dict.items())
     pattern = re.compile("|".join(replacements.keys()))
     result_text = pattern.sub(lambda m: replacements[re.escape(m.group(0))], text)
     return result_text
+
 
 def load_private_metadata_to_modal_text(modal_text, req_meta):
     modal_json = json.loads(modal_text)
@@ -102,7 +121,7 @@ def load_private_metadata_to_modal_text(modal_text, req_meta):
 def handle_submission(ack, body, view, respond):
     try:
         metadata = get_private_metadata_from_view(view)
-        req_meta = RequestMetadata.from_dict(metadata)
+        req_meta = RequestMetadata.from_string(metadata)
 
     except KeyError as e:
         errors = {}
@@ -114,7 +133,9 @@ def handle_submission(ack, body, view, respond):
 
     chosen_reactions = get_reactions_chosen_in_main(view)
 
-    req_meta.users_to_ping = get_users_not_reacted_to_any_reactions(req_meta, chosen_reactions)
+    req_meta.users_to_ping = get_users_not_reacted_to_any_reactions(
+        req_meta, chosen_reactions
+    )
 
     chosen_actions = get_actions_chosen_from_view(view)
 
@@ -130,34 +151,46 @@ def handle_submission(ack, body, view, respond):
         trigger_id = body["trigger_id"]
         open_ping_modal(trigger_id, req_meta)
 
-        req_meta.summary += '''\nOtworzyłem okienko ping\n'''
+        req_meta.summary += """\nOtworzyłem okienko ping\n"""
 
     if "SEND_DM_NOT_REACTED" in chosen_actions:
         trigger_id = body["trigger_id"]
         open_dm_modal(trigger_id, req_meta)
-        
-        req_meta.summary += '''Otworzyłem okienko wysyłania wiadomości :yum:\n'''
 
-    respond(response_type="ephemeral", text=req_meta.summary)
+        req_meta.summary += """Otworzyłem okienko wysyłania wiadomości :yum:\n"""
+
+    app.client.chat_postEphemeral(
+        channel=req_meta.channel, text=req_meta.summary, user=req_meta.user
+    )
+
     print("Pomyslnie wykonano zliczanie reakcji")
+
 
 def get_private_metadata_from_view(view):
     if not "private_metadata" in view:
         raise KeyError("Key 'private_metadata' not found!")
-    
-    return view['private_metadata']
+
+    return view["private_metadata"]
+
 
 def get_reactions_chosen_in_main(view):
     chosen_reactions = []
-    for reaction in view["state"]["values"]["SELECT_REACTIONS"]["REACTIONS_LIST"]["selected_options"]:
+    for reaction in view["state"]["values"]["SELECT_REACTIONS"]["REACTIONS_LIST"][
+        "selected_options"
+    ]:
         chosen_reactions.append(reaction["value"])
 
     return chosen_reactions
 
+
 def summary_reactions(reactions_chosen, req_meta: RequestMetadata):
     reaction_counter = 0
     summary = ""
-    reactions = [reaction_type for reaction_type in req_meta.reactions if reaction_type['name'] in reactions_chosen]
+    reactions = [
+        reaction_type
+        for reaction_type in req_meta.reactions
+        if reaction_type["name"] in reactions_chosen
+    ]
 
     for reaction in reactions:
         reaction_counter += 1
@@ -165,26 +198,34 @@ def summary_reactions(reactions_chosen, req_meta: RequestMetadata):
 
     return summary
 
+
 def summary_not_reacted(req_meta: RequestMetadata):
     summary = ""
 
     if len(req_meta.users_to_ping) > 0:
         summary += "Niewdzięcznicy którzy nie odpowiedzieli to:\n"
         summary += get_users_text_with_links(req_meta.users_to_ping)
-    
+
     else:
         summary += "Ja pierdziu ale Ty masz posłuch, każdy odpowiedział!!! :party_blob:"
 
     return summary
 
+
 def get_reaction_single_summary(no_reaction, reaction):
-    line = str(no_reaction) + ". Reakcja: :" + str(reaction['name']) \
-                + ": \n\t- Ilość odpowiedzi: " + str(reaction['count']) \
-                + "\n\t- Kto głosował:\n"
-    
+    line = (
+        str(no_reaction)
+        + ". Reakcja: :"
+        + str(reaction["name"])
+        + ": \n\t- Ilość odpowiedzi: "
+        + str(reaction["count"])
+        + "\n\t- Kto głosował:\n"
+    )
+
     line += get_users_text_with_links(reaction["users"])
-    
+
     return line
+
 
 def get_users_text_with_links(users):
     summary = ""
@@ -192,63 +233,82 @@ def get_users_text_with_links(users):
         summary += "\t\t<@" + str(user) + ">\n"
     return summary
 
+
 def get_users_not_reacted_to_any_reactions(req_meta: RequestMetadata, reactions):
     req_meta.users_to_ping = req_meta.members
 
-    reactions = [reaction_type for reaction_type in req_meta.reactions if reaction_type['name'] in reactions]
+    reactions = [
+        reaction_type
+        for reaction_type in req_meta.reactions
+        if reaction_type["name"] in reactions
+    ]
 
     for reaction in reactions:
         remove_from_ping(reaction, req_meta)
-    
-    req_meta.users_to_ping = [user for user in req_meta.users_to_ping if not user in STRINGS_USER["bots_id"]]
-    
+
+    req_meta.users_to_ping = [
+        user for user in req_meta.users_to_ping if not user in STRINGS_USER["bots_id"]
+    ]
+
     return req_meta.users_to_ping
+
 
 def remove_from_ping(reaction, req_meta):
     for user in reaction["users"]:
         if user in req_meta.users_to_ping:
             req_meta.users_to_ping.remove(user)
 
+
 def get_actions_chosen_from_view(view):
     chosen_options = []
-    for option in view["state"]["values"]["SELECT_OPTIONS"]["checkboxes-action"]["selected_options"]:
+    for option in view["state"]["values"]["SELECT_OPTIONS"]["checkboxes-action"][
+        "selected_options"
+    ]:
         chosen_options.append(option["value"])
 
     return chosen_options
 
+
 def open_ping_modal(trigger_id, req_meta: RequestMetadata):
     initial_users = get_users_for_initial_choice
 
-    replacements = {"{{link_to_message}}": f"{req_meta.link_to_message}",
-                    "{{initial_users}}": f"{initial_users}"}
-    
+    replacements = {
+        "{{link_to_message}}": f"{req_meta.link_to_message}",
+        "{{initial_users}}": f"{initial_users}",
+    }
+
     view = load_modal(STRINGS_UTILS["modals"]["ping"]["file"], replacements)
 
     app.client.views_open(trigger_id=trigger_id, view=view)
     print("Otwarto okienko ping")
 
+
 def open_dm_modal(trigger_id, req_meta: RequestMetadata):
     initial_users = get_users_for_initial_choice(req_meta)
 
-    replacements = {"{{link_to_message}}": f"{req_meta.link_to_message}",
-                    "{{initial_users}}": f"{initial_users}"}
-    
+    replacements = {
+        "{{link_to_message}}": f"{req_meta.link_to_message}",
+        "{{initial_users}}": f"{initial_users}",
+    }
+
     view = load_modal("dm_modal.json", replacements)
 
     app.client.views_open(trigger_id=trigger_id, view=view)
     print("Otwarto okienko dm")
+
 
 def get_users_for_initial_choice(users):
     initial_users = ""
     count = 0
     for user in users:
         if count > 0:
-            initial_users += ''',
-            '''
+            initial_users += """,
+            """
         initial_users += '"' + str(user) + '"'
         count += 1
 
     return initial_users
+
 
 @app.view(STRINGS_UTILS["modals"]["ping"]["id"])
 def handle_ping_submission(ack, body, view):
@@ -260,20 +320,32 @@ def handle_ping_submission(ack, body, view):
     ping_users(users_to_ping, link_to_message, user_pinging)
     print("Zamknięto okienko pingu")
 
+
 def get_users_to_ping_from_view(view):
     users_to_ping = []
-    for user in view["state"]["values"]["SELECT_TO_PING"]["USERS_LIST"]["selected_conversations"]:
+    for user in view["state"]["values"]["SELECT_TO_PING"]["USERS_LIST"][
+        "selected_conversations"
+    ]:
         users_to_ping.append(user)
     return users_to_ping
 
+
 def ping_users(users, message_link, user_pinging):
     if len(users) > 0:
-        ping_message = "Ping na życzenie <@" + str(user_pinging) + ">)\nDelikwenci którzy nie odpowiedzieli na wiadomość: " + \
-            str(message_link) + " to:\n"
-        
+        ping_message = (
+            "Ping na życzenie <@"
+            + str(user_pinging)
+            + ">)\nDelikwenci którzy nie odpowiedzieli na wiadomość: "
+            + str(message_link)
+            + " to:\n"
+        )
+
         ping_message += get_users_text_with_links(users)
 
-    app.client.chat_postMessage(channel=STRINGS_USER["ping_channel_name"], text=ping_message)
+    app.client.chat_postMessage(
+        channel=STRINGS_USER["ping_channel_name"], text=ping_message
+    )
+
 
 @app.view(STRINGS_UTILS["modals"]["dm"]["id"])
 def handle_dm_submission(client, ack, body, view):
@@ -285,23 +357,31 @@ def handle_dm_submission(client, ack, body, view):
     send_dm_to_users(client, users_to_dm, link_to_message, user_reminding)
     print("Zamknięto okienko DM")
 
+
 def get_users_to_dm_from_view(view):
     users_to_dm = []
-    for user in view["state"]["values"]["SELECT_TO_DM"]["USERS_LIST"]["selected_conversations"]:
+    for user in view["state"]["values"]["SELECT_TO_DM"]["USERS_LIST"][
+        "selected_conversations"
+    ]:
         users_to_dm.append(user)
-    
+
     return users_to_dm
 
 
 def send_dm_to_users(client, users_to_ping, link_to_message, user_reminding):
-    text = '''
-        Siemanko kochany WRSS'owiczu! :wave: Dostałem zadanie od <@''' + user_reminding + '''> przypomnieć Ci żeby odpowiedzieć na wiadomość, która znajduje się tutaj: 
-        ''' + link_to_message + '''
+    text = (
+        """
+        Siemanko kochany WRSS'owiczu! :wave: Dostałem zadanie od <@"""
+        + user_reminding
+        + """> przypomnieć Ci żeby odpowiedzieć na wiadomość, która znajduje się tutaj: 
+        """
+        + link_to_message
+        + """
     Wiem, że pewnie masz niesłychanie dużo pracy, ale myślę, że pójdzie Ci to szybko :wink:
-    A z tego co wiem, lepiej nie lądować na kanale *#ping* :woozy_face:'''
+    A z tego co wiem, lepiej nie lądować na kanale *#ping* :woozy_face:"""
+    )
     for user in users_to_ping:
-        client.chat_postMessage(
-            channel=user, text=text)
+        client.chat_postMessage(channel=user, text=text)
 
 
 if __name__ == "__main__":
--- utils/requests.py
+++ utils/requests.py
@@ -1,41 +1,64 @@
+import json
+
+
 class RequestMetadata:
-    def __init__(self, link_to_message, reactions, summary, members, users_to_ping):
+    def __init__(
+        self, link_to_message, reactions, summary, members, users_to_ping, channel, user
+    ):
         self.link_to_message = link_to_message
         self.reactions = reactions
         self.summary = summary
         self.members = members
         self.users_to_ping = users_to_ping
+        self.channel = channel
+        self.user = user
 
     @staticmethod
     def from_payload(client, payload):
+        channel = payload["channel"]["id"]
+        user = payload["user"]["id"]
+
         link_to_message = client.chat_getPermalink(
-            channel=payload["channel"]["id"], message_ts=payload["message_ts"])["permalink"]
-        
+            channel=channel, message_ts=payload["message_ts"]
+        )["permalink"]
+
         try:
             reactions_response = client.reactions_get(
-                timestamp=payload["message_ts"], full=True, channel=payload["channel"]["id"])
+                timestamp=payload["message_ts"],
+                full=True,
+                channel=channel,
+            )
             reactions = reactions_response["message"]["reactions"]
         except KeyError:
             reactions = []
 
-        channel_members = client.conversations_members(channel=payload["channel"]["id"])["members"]
+        channel_members = client.conversations_members(channel=channel)["members"]
 
-        summary = "*Podsumowanie reakcji na wiadomość " + \
-            str(link_to_message) + " *\n"
-        
-        users_to_ping = set([])
+        summary = "*Podsumowanie reakcji na wiadomość " + str(link_to_message) + " *\n"
 
-        return RequestMetadata(link_to_message, reactions, summary, channel_members, users_to_ping)
-    
+        users_to_ping = {}
+
+        return RequestMetadata(
+            link_to_message,
+            reactions,
+            summary,
+            channel_members,
+            users_to_ping,
+            channel,
+            user,
+        )
+
     def to_dict(self):
         return {
             "link_to_message": self.link_to_message,
             "reactions": self.reactions,
             "summary": self.summary,
             "members": self.members,
-            "users_to_ping": self.users_to_ping
+            "users_to_ping": self.users_to_ping,
+            "channel": self.channel,
+            "user": self.user,
         }
-    
+
     @staticmethod
     def from_dict(dict):
         return RequestMetadata(
@@ -43,6 +66,16 @@ class RequestMetadata:
             dict["reactions"],
             dict["summary"],
             dict["members"],
-            dict["users_to_ping"]
+            dict["users_to_ping"],
+            dict["channel"],
+            dict["user"],
         )
-        
+
+    def to_string(self):
+        self_json = json.dumps(self.to_dict())
+
+        return self_json
+
+    @staticmethod
+    def from_string(str_self):
+        return RequestMetadata.from_dict(json.loads(str_self))
